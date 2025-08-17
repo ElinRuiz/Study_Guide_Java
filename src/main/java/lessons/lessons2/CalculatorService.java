e8140bb7a2fa97bd0e0ac423f41f5023d09e89ed
Elin Ruiz <63513938+ElinRuiz@users.noreply.github.com>
2025-08-17 11:51:00 -0500

    Fix file name

--- src/main/java/lessons/lessons2/CalculatorService.java
+++ src/main/java/lessons/lessons2/CalculatorService.java
@@ -3,7 +3,7 @@ package lessons.lessons2;
 public class CalculatorService {
 
     public int add(int a, int b) {
-        return a + b;
+          return a + b;
     }
 
     public int subtract(int a, int b) {
